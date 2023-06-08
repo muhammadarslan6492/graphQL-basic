974376e77f1f27a8b999e0b391c6f166b44a4364
muhammad arslan <muhammadarslan6492@gmail.com>
2023-06-08 13:51:00 -0700

    feat: Working on operational arguments in graphql

--- src/index.js
+++ src/index.js
@@ -2,30 +2,49 @@ import { GraphQLServer } from 'graphql-yoga';
 
 // type definition (schema)
 
+// const typeDefs = `type Query {
+//     product: Product!
+//     }
+//    type Product {
+//     id: ID!
+//     title: String!
+//     price: Float!
+//     releaseYear: Int
+//     rating: Float
+//     inStock: Boolean!
+// }`;
+
+// Operational Arguments
+
 const typeDefs = `type Query {
-    product: Product!
-    }
-   type Product {
-    id: ID!
-    title: String!
-    price: Float!
-    releaseYear: Int
-    rating: Float
-    inStock: Boolean!
+  greeting(name: String): String!
 }`;
+
 // Resolvers
+
+// const resolvers = {
+//   Query: {
+//     product() {
+//       return {
+//         id: 12,
+//         title: 'test title',
+//         price: 100,
+//         releaseYear: 2023,
+//         rating: 4.5,
+//         inStock: true,
+//       };
+//     },
+//   },
+// };
 
 const resolvers = {
   Query: {
-    product() {
-      return {
-        id: 12,
-        title: 'test title',
-        price: 100,
-        releaseYear: 2023,
-        rating: 4.5,
-        inStock: true,
-      };
+    greeting(parent, args, ctx, info) {
+      if (args.name) {
+        return `Hello, ${args.name}`;
+      } else {
+        return 'Hello!';
+      }
     },
   },
 };
